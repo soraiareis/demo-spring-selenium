6363eb1912cea685c89c461d9f3c035346bfddaf
Soraia Fernandes <soraia_fernandes@gap.com>
2020-08-01 23:12:49 -0700

    Adding Reporting, Screenshots and Logging

--- src/test/java/demo/spring/selenium/CucumberRunner.java
+++ src/test/java/demo/spring/selenium/CucumberRunner.java
@@ -5,7 +5,12 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
     glue = "demo/spring/selenium/stepdefinitions",
-    features = "src/test/resources/features")
+    features = "src/test/resources/features",
+    plugin = {
+        "pretty",
+        "html:build/test-results/html-report.html",
+        "json:build/test-results/json-report.json"
+    })
 public class CucumberRunner extends AbstractTestNGCucumberTests {
 
 }
--- src/test/java/demo/spring/selenium/stepdefinitions/Hooks.java
+++ src/test/java/demo/spring/selenium/stepdefinitions/Hooks.java
@@ -4,13 +4,18 @@ import demo.spring.selenium.SpringContextConfiguration;
 import demo.spring.selenium.config.DemoSpringSeleniumProperties;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
 import io.cucumber.spring.CucumberContextConfiguration;
+import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @CucumberContextConfiguration
 @SpringBootTest(classes = {SpringContextConfiguration.class})
+@Slf4j
 public class Hooks {
 
   @Autowired
@@ -20,13 +25,17 @@ public class Hooks {
   private WebDriver driver;
 
   @Before
-  public void openBrowser() {
+  public void openBrowser(Scenario scenario) {
     driver.get(properties.getHost());
+    log.info("[STARTED] Scenario: " + scenario.getName());
   }
 
   @After
-  public void closeBrowser() {
+  public void closeBrowser(Scenario scenario) {
+    byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+    scenario.attach(screenshot, "image/png", scenario.getName());
     driver.quit();
+    log.info("[ENDED] Scenario: " + scenario.getName());
   }
 }
 
